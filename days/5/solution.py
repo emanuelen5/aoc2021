c17e6e362c32b2ed5d7abb0914fc0c20046d74ea
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-12-12 11:44:51 +0100

    Starting on day 5 part 2

--- days/5/solution.py
+++ days/5/solution.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from pathlib import Path
 import numpy as np
 import re
 
@@ -18,6 +19,15 @@ class Line:
     def is_vertical(self) -> bool:
         return self.x1 == self.x2
 
+    def is_diagonal_down(self) -> bool:
+        return (self.x2 - self.x1) == (self.y2 - self.y1)
+
+    def is_diagonal_up(self) -> bool:
+        return (self.x2 - self.x1) == -(self.y2 - self.y1)
+
+    def length(self) -> int:
+        return max(abs(self.x2 - self.x1) + 1, abs(self.y2 - self.y1) + 1)
+
     @classmethod
     def from_line(cls, line: str):
         m = re_pattern.match(line)
@@ -28,20 +38,42 @@ class Line:
             int(m.group(4)),
         )
 
+    def __repr__(self):
+        return f"<Line ({self.x1},{self.y1}) -> ({self.x2},{self.y2})>"
 
-with open("data.txt") as f:
+
+with open(Path(__file__).parent / "data.txt") as f:
     lines = [Line.from_line(line) for line in f.readlines()]
 
-grid = np.zeros((1000, 1000))
+grid1 = np.zeros((1000, 1000), np.uint16)
+grid2 = np.zeros((1000, 1000), np.uint16)
 for line in lines:
+    min_y = min(line.y1, line.y2)
+    max_y = max(line.y1, line.y2)
+    min_x = min(line.x1, line.x2)
+    max_x = max(line.x1, line.x2)
     if line.is_vertical():
-        min_y = min(line.y1, line.y2)
-        max_y = max(line.y1, line.y2)
-        grid[min_y:max_y + 1, line.x1] += 1
+        grid1[min_y:max_y + 1, line.x1] += 1
+        grid2[min_y:max_y + 1, line.x1] += 1
     elif line.is_horizontal():
-        min_x = min(line.x1, line.x2)
-        max_x = max(line.x1, line.x2)
-        grid[line.y1, min_x:max_x + 1] += 1
+        grid1[line.y1, min_x:max_x + 1] += 1
+        grid2[line.y1, min_x:max_x + 1] += 1
+    elif line.is_diagonal_down():
+        for x, y in zip(range(min_x, max_x), range(min_y, max_y)):
+            grid2[y, x] += 1
+    elif line.is_diagonal_up():
+        for x, y in zip(range(min_x, max_x), range(max_y, min_y, -1)):
+            grid2[y, x] += 1
+    else:
+        print(line)
 
-atleast_2 = np.count_nonzero(grid >= 2)
-print(f"Part 1: At least two overlaps {atleast_2}")
+atleast_2_part1 = np.count_nonzero(grid1 >= 2)
+atleast_2_part2 = np.count_nonzero(grid2 >= 2)
+print(f"Part 1: At least two overlaps {atleast_2_part1}")
+print(f"Part 2: At least two overlaps {atleast_2_part2}")
+
+print(np.sum(grid2))
+print(sum(line.length() for line in lines))
+
+# Guesses
+# 20639 too low
