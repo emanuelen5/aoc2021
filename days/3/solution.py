2ed3a3f80485824313de0a62456380289a337250
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-12-13 23:25:58 +0100

    Completing day 3

--- days/3/solution.py
+++ days/3/solution.py
@@ -27,3 +27,37 @@ epsilon = array2bin(inverted_bit_averages)
 
 print("Part 1 gamma x epsilon = power")
 print(f"       {gamma} x {epsilon} = {gamma * epsilon}")
+
+
+def bit_condition(vals: np.ndarray, type_: int):
+    if type_ == 0:
+        return 1 if vals.mean() >= 0.5 else 0
+    return 0 if vals.mean() >= 0.5 else 1
+
+
+bit_patterns2 = bit_patterns.copy()
+for i in range(bit_count):
+    search_pattern = bit_condition(bit_patterns2[:, i], 0)
+    mask = bit_patterns2[:, i] == search_pattern
+    bit_patterns2 = bit_patterns2[mask, :]
+    if len(bit_patterns2) == 1:
+        oxygen = array2bin(bit_patterns2[0])
+        break
+else:
+    raise ValueError("Ran to end without finding answer")
+
+
+bit_patterns2 = bit_patterns.copy()
+for i in range(bit_count):
+    search_pattern = bit_condition(bit_patterns2[:, i], 1)
+    mask = bit_patterns2[:, i] == search_pattern
+    bit_patterns2 = bit_patterns2[mask, :]
+    if len(bit_patterns2) == 1:
+        co2 = array2bin(bit_patterns2[0])
+        break
+else:
+    raise ValueError("Ran to end without finding answer")
+
+
+print("Part 2 oxygen x CO2 = life support")
+print(f"       {oxygen} x {co2} = {oxygen * co2}")
