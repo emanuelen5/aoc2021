00fb92ba4f5af998e22816703183a5afd896e795
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-12-13 22:46:55 +0100

    Refactor: 3 part 1 to use numpy

--- days/3/solution.py
+++ days/3/solution.py
@@ -1,23 +1,29 @@
-with open("data.txt") as f:
+import numpy as np
+from pathlib import Path
+
+
+with open(Path(__file__).parent / "data.txt") as f:
     lines = [line.strip() for line in f.readlines()]
 
-
 bit_count = len(lines[0])
-bit_averages = []
+bit_patterns = np.zeros((len(lines), bit_count), dtype=np.uint8)
+for i, line in enumerate(lines):
+    for j, bit in enumerate(line):
+        bit_patterns[i][j] = bit == "1"
 
-gamma = 0
-for bit_index in range(bit_count):
-    bits = [int(line[bit_index]) for line in lines]
-    common_value = max([0, 1], key = lambda k: bits.count(k))
-    bit_averages.append(common_value)
+bit_averages = bit_patterns.mean(axis=0).round().astype(np.uint8)
 
-
-inverted_bit_averages = [1-i for i in bit_averages]
+inverted_bit_averages = 1 - bit_averages
 print(f"bit averages: {bit_averages}")
 print(f"inverted    : {inverted_bit_averages}")
 
-gamma = int("".join(str(i) for i in bit_averages), 2)
-epsilon = int("".join(str(i) for i in inverted_bit_averages), 2)
+
+def array2bin(arr: np.ndarray) -> int:
+    return int("".join(str(i) for i in arr), 2)
+
+
+gamma = array2bin(bit_averages)
+epsilon = array2bin(inverted_bit_averages)
 
 print("Part 1 gamma x epsilon = power")
 print(f"       {gamma} x {epsilon} = {gamma * epsilon}")
