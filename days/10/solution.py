a2b7808d9f4a42ff8ec92d3b066a6ab1865aa4c1
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-12-19 00:10:32 +0100

    Solution to day 10 part 2

--- days/10/solution.py
+++ days/10/solution.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from pathlib import Path
+import numpy as np
 
 parser = ArgumentParser()
 parser.add_argument("--input-file", "-i", default=Path(__file__).parent / "data.txt")
@@ -10,8 +11,10 @@ with open(args.input_file) as f:
 
 parens_equivalents = {'(': ')', '{': '}', '[': ']', '<': '>'}
 parens_score = {')': 3, '}': 1197, ']': 57, '>': 25137}
+completion_score = {')': 1, ']': 2, '}': 3, '>': 4}
 
-line_uncomplete_score = []
+line_uncomplete_scores = []
+line_completion_scores = []
 for line in lines:
     parens_stack = []
     for char in line:
@@ -20,7 +23,16 @@ for line in lines:
         elif char == parens_equivalents[parens_stack[-1]]:
             parens_stack.pop()
         else:
-            line_uncomplete_score.append(parens_score[char])
+            line_uncomplete_scores.append(parens_score[char])
             break
+    else:
+        missing_parens = [parens_equivalents[p] for p in reversed(parens_stack)]
+        score = 0
+        for p in missing_parens:
+            score *= 5
+            score += completion_score[p]
+        line_completion_scores.append(score)
+        print(score)
 
-print(f"Part 1: {sum(line_uncomplete_score)=}")
+print(f"Part 1: {sum(line_uncomplete_scores)=}")
+print(f"Part 2: {int(np.median(line_completion_scores))=}")
