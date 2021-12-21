b884eb8ae378dbb8384e0955fc3f93555a7e77cd
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-12-21 21:30:27 +0100

    Solution to day 12 part 2

--- days/12/solution.py
+++ days/12/solution.py
@@ -20,20 +20,26 @@ for mapping in mappings:
     connections[mapping[1]].append(mapping[0])
 
 visits = {c: 0 for c in connections.keys()}
+visits["start"] = 2  # So we cannot return here
 
 
-def explore_paths(from_node: str) -> int:
+def explore_paths(from_node: str, visit_order: list[str], visit_twice: bool = False) -> int:
     if from_node == "end":
+        # print(",".join(visit_order))
         return 1
     visits[from_node] += 1
     sum_ = 0
     for to_node in connections[from_node]:
         if to_node.upper() == to_node or visits[to_node] < 1:
-            sum_ += explore_paths(to_node)
+            sum_ += explore_paths(to_node, visit_order + [to_node], visit_twice)
+        if visit_twice and to_node.lower() == to_node and visits[to_node] == 1:
+            sum_ += explore_paths(to_node, visit_order + [to_node], False)
     visits[from_node] -= 1
     return sum_
 
 
-paths = explore_paths("start")
+paths = explore_paths("start", ["start"])
+print(f"Part 1: {paths}\n")
 
-print(f"Part 1: {paths}")
+paths = explore_paths("start", ["start"], visit_twice=True)
+print(f"Part 2: {paths}")
