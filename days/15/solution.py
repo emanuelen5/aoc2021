0fbedda50074f4b273c24ef2721cbd8e9e58b603
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-12-26 22:43:06 +0100

    Solution to day 15 part 2 in short time

--- days/15/solution.py
+++ days/15/solution.py
@@ -13,30 +13,32 @@ with open(args.input_file) as f:
 
 width = len(lines[0].strip())
 height = len(lines)
-risks = np.zeros((height * 5, width * 5))
-for ii in range(5):
-    for jj in range(5):
-        for i, line in enumerate(lines):
-            for j in range(width):
-                risks[i + ii * height, j + jj * width] = ((int(line[j]) + ii + jj - 1) % 9) + 1
 
-total_risks = np.zeros_like(risks)
-total_risks[:] = float('inf')
-exhausted = {(0, 0)}
-to_visit = [(0, 0, 0)]
+for end_x, end_y in ((width - 1, height - 1), (width*5 - 1, height*5 - 1)):
+    risks = np.zeros((height * 5, width * 5))
+    for ii in range(5):
+        for jj in range(5):
+            for i, line in enumerate(lines):
+                for j in range(width):
+                    risks[i + ii * height, j + jj * width] = ((int(line[j]) + ii + jj - 1) % 9) + 1
 
-total_risks[0, 0] = 0
+    exhausted = {(0, 0)}
+    to_visit = [(0, 0, 0)]
 
-while to_visit:
-    risk, y, x = heappop(to_visit)
+    i = 0
+    to_visit_lookup = {(0, 0): 0}
+    while to_visit:
+        risk, y, x = heappop(to_visit)
+        to_visit_lookup.pop((y, x), None)
+        if x == end_x and y == end_y:
+            print(f"Total risk: {risk:.0f}")
+            break
 
-    for y_, x_ in ([y, x-1], [y+1, x], [y, x+1], [y-1, x]):
-        if (y_, x_) not in exhausted and (0 <= x_ <= width * 5 - 1) and (0 <= y_ <= height * 5 - 1):
-            total_risk = risks[y_, x_] + risk
-            if total_risk < total_risks[y_, x_]:
-                total_risks[y_, x_] = total_risk
-            heappush(to_visit, (total_risk, y_, x_))
-            exhausted.add((y, x))
-
-print(f"Part 1: {total_risks[height - 1, width - 1]:.0f}\n")
-print(f"Part 2: {total_risks[height * 5 - 1, width * 5 - 1]:.0f}\n")
+        for y_, x_ in ([y, x-1], [y+1, x], [y, x+1], [y-1, x]):
+            if (y_, x_) not in exhausted and (0 <= x_ <= end_x) and (0 <= y_ <= end_y):
+                total_risk = risks[y_, x_] + risk
+                if total_risk < to_visit_lookup.get((y_, x_), float('inf')):
+                    heappush(to_visit, (total_risk, y_, x_))
+                    to_visit_lookup[(y_, x_)] = total_risk
+                exhausted.add((y, x))
+        i += 1
