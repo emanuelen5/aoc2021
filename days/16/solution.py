32516c7d5a7b6e511ff1680bd758d25e0a9c224f
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-12-26 12:52:27 +0100

    Solution to day 16 part 2

--- days/16/solution.py
+++ days/16/solution.py
@@ -21,37 +21,59 @@ def deconstruct(s: str):
             const += s[length+1:length+5]
             length += 5
         const = int(const, 2)
-        print(f"Constant packet: {const=}")
-        return length, version
+        return length, version, const
     else:
         length_id = s[6]
+        subpacket_values = []
         if length_id == "0":
             length = 7 + 15
             subpacket_len = int(s[7:length], 2)
             parsed_len = 0
             while parsed_len < subpacket_len:
-                print(f"Subpacket {parsed_len}/{subpacket_len} length {{")
-                length_, version_ = deconstruct(s[length + parsed_len:])
-                print("}")
+                length_, version_, value = deconstruct(s[length + parsed_len:])
+                subpacket_values.append(value)
                 parsed_len += length_
                 version = version + version_
-            return length + parsed_len, version
+            length = length + parsed_len
         else:
             length = 7 + 11
             subpacket_count = int(s[7:length], 2)
             for i in range(subpacket_count):
-                print(f"Subpacket {i+1}/{subpacket_count} count {{")
-                length_, version_ = deconstruct(s[length:])
-                print("}")
+                length_, version_, value = deconstruct(s[length:])
+                subpacket_values.append(value)
                 version += version_
                 length += length_
-            return length, version
+        if type_id == 0:
+            return length, version, sum(subpacket_values)
+        elif type_id == 1:
+            product = subpacket_values[0]
+            for p in subpacket_values[1:]:
+                product *= p
+            return length, version, product
+        elif type_id == 2:
+            return length, version, min(subpacket_values)
+        elif type_id == 3:
+            return length, version, max(subpacket_values)
+        elif type_id == 5:
+            if len(subpacket_values) != 2:
+                raise RuntimeError(f"Wrong number of subpackets. Got {len(subpacket_values)}")
+            return length, version, 1 if subpacket_values[0] > subpacket_values[1] else 0
+        elif type_id == 6:
+            if len(subpacket_values) != 2:
+                raise RuntimeError(f"Wrong number of subpackets. Got {len(subpacket_values)}")
+            return length, version, 1 if subpacket_values[0] < subpacket_values[1] else 0
+        elif type_id == 7:
+            if len(subpacket_values) != 2:
+                raise RuntimeError(f"Wrong number of subpackets. Got {len(subpacket_values)}")
+            return length, version, 1 if subpacket_values[0] == subpacket_values[1] else 0
+        raise RuntimeError(f"Logic error. Got packet type {type_id}")
 
 
 for line in lines:
     data = bytearray.fromhex(line)
-    bits = "".join(f"{d:08b}" for d in data).rstrip("0")
-    length, version = deconstruct(bits)
+    bits = "".join(f"{d:08b}" for d in data)
+    length, version, value = deconstruct(bits)
+    print(f"\nPacket: {line}")
+    print(f"\tbits: {bits}")
     print(f"Part 1. Version sum: {version}")
-
-print(f"Part 2: {0}\n")
+    print(f"Part 2: Value: {value}\n")
