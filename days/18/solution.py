Fixing rotation test for day 19

days/18/solution.py
@@ -4,13 +4,6 @@ from pathlib import Path
 import numpy as np
 from snailnumber import Snailnumber
 
-parser = ArgumentParser()
-parser.add_argument("--input-file", "-i", default=Path(__file__).parent / "data.txt")
-args = parser.parse_args()
-
-with open(args.input_file) as f:
-    lists = [json.loads(line) for line in f.read().splitlines()]
-
 
 def add_from_list(lists):
     sn = Snailnumber.from_list(lists[0])
@@ -19,18 +12,26 @@ def add_from_list(lists):
     return sn
 
 
-print(f"Part 1: {add_from_list(lists).calc_magnitude()}\n")
-
-
 def addition_combinations(lists):
     combination_matrix = np.zeros((len(lists), len(lists)), dtype=int)
     for i, list1 in enumerate(lists):
-        for j, list2 in enumerate(lists[i+1:]):
-            combination_matrix[i, j+i+1] = (Snailnumber.from_list(list1) + Snailnumber.from_list(list2)).calc_magnitude()
+        for j, list2 in enumerate(lists[i + 1:]):
+            combination_matrix[i, j + i + 1] = (
+                    Snailnumber.from_list(list1) + Snailnumber.from_list(list2)).calc_magnitude()
     return combination_matrix
 
 
-np.set_printoptions(threshold=np.inf)
-res = addition_combinations(lists)
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--input-file", "-i", default=Path(__file__).parent / "data.txt")
+    args = parser.parse_args()
 
-print(f"Part 2: {np.amax(res, axis=None)}\n")
+    with open(args.input_file) as f:
+        lists = [json.loads(line) for line in f.read().splitlines()]
+
+    print(f"Part 1: {add_from_list(lists).calc_magnitude()}\n")
+
+    np.set_printoptions(threshold=np.inf)
+    res = addition_combinations(lists)
+
+    print(f"Part 2: {np.amax(res, axis=None)}\n")

days/19/scanning.py
@@ -6,17 +6,17 @@ from functools import cache
 
 @cache
 def create_rot_mat(alpha: float, beta: float, gamma: float):
-    rot_x = np.ndarray([
+    rot_x = np.array([
         [1, 0, 0],
         [0, math.cos(alpha), -math.sin(alpha)],
         [0, math.sin(alpha), math.cos(alpha)]
     ])
-    rot_y = np.ndarray([
+    rot_y = np.array([
         [math.cos(beta), 0, math.sin(beta)],
         [0, 1, 0],
         [-math.sin(beta), 0, math.cos(beta)]
     ])
-    rot_z = np.ndarray([
+    rot_z = np.array([
         [math.cos(gamma), -math.sin(gamma), 0],
         [math.sin(gamma), math.cos(gamma), 0],
         [0, 0, 1]
@@ -32,12 +32,39 @@ class Scanning:
 
     def rotate(self, alpha: float, beta: float, gamma: float):
         """ Rotate in degrees around either axis """
-        rot_mat = create_rot_mat(alpha, beta, gamma)
-        scans = self.scans * rot_mat
-        return self.__class__(scans)
+        rot_mat = create_rot_mat(alpha * math.pi/180, beta * math.pi/180, gamma * math.pi/180)
+        scans = np.matmul(self.scans, rot_mat)
+        return self.__class__(scans, self.id)
 
     def create_rotation_permutations(self) -> list["Scanning"]:
         pass
 
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return np.array_equiv(self.scans, other.scans) and self.id == other.id
+
     def __repr__(self):
         return f"<Scanning n_scans={len(self.scans)} id={self.id}>"
+
+    @classmethod
+    def read_file(cls, filename: str) -> list["Scanning"]:
+        import re
+        with open(filename) as f:
+            lines = f.read().splitlines()
+        lines = [line for line in lines if len(line)]
+
+        scan_pattern = re.compile(r"^--- scanner (\d+) ---$")
+        scanning_values = id_ = None
+        scannings = []
+        for line in lines:
+            if m := scan_pattern.match(line):
+                if scanning_values is not None:
+                    scanning_values = np.array(scanning_values)
+                    scanning = Scanning(scanning_values, id_)
+                    scannings.append(scanning)
+                id_ = int(m.group(1))
+                scanning_values = []
+                continue
+            scanning_values.append([int(v) for v in line.split(",")])
+        return scannings

days/19/solution.py
@@ -1,32 +1,14 @@
 from argparse import ArgumentParser
-import numpy as np
 from pathlib import Path
-import re
 from scanning import Scanning
 
-parser = ArgumentParser()
-parser.add_argument("--input-file", "-i", default=Path(__file__).parent / "data.txt")
-args = parser.parse_args()
 
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--input-file", "-i", default=Path(__file__).parent / "data.txt")
+    args = parser.parse_args()
 
-with open(args.input_file) as f:
-    lines = f.read().splitlines()
-lines = [line for line in lines if len(line)]
+    scannings = Scanning.read_file(args.input_file)
 
-
-scan_pattern = re.compile(r"^--- scanner (\d+) ---$")
-scanning_values = id_ = None
-scannings = []
-for line in lines:
-    if m := scan_pattern.match(line):
-        if scanning_values is not None:
-            scanning_values = np.array(scanning_values)
-            scanning = Scanning(scanning_values, id_)
-            scannings.append(scanning)
-        id_ = int(m.group(1))
-        scanning_values = []
-        continue
-    scanning_values.append([int(v) for v in line.split(",")])
-
-print(f"Part 1: {0}\n")
-print(f"Part 2: {0}\n")
+    print(f"Part 1: {0}\n")
+    print(f"Part 2: {0}\n")