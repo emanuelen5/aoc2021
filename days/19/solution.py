8c0abca4d0d933d20b6f2200d40ed7f807120055
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-01-16 19:22:40 +0100

    Continuing a bit

--- days/19/solution.py
+++ days/19/solution.py
@@ -1,14 +1,15 @@
 from argparse import ArgumentParser
 from pathlib import Path
 from scanning import Scanning, _find_jumps_to_scanner
+import numpy as np
 
 
-def find_unique_nodes(scannings: Scanning, result: list[tuple[int, int, tuple[int, int, int], tuple[int, int, int]]]) -> int:
+def find_jumps(scannings: Scanning, result: list[tuple[int, int, tuple[int, int, int], tuple[int, int, int]]]) -> int:
     all_mappings = set(tuple(sorted((scanner1, scanner2))) for scanner1, scanner2, *_ in result)
+    jumps = dict()
     for i in range(len(scannings)):
-        jumps = _find_jumps_to_scanner(all_mappings, i)
-        print(jumps)
-    return 0
+        jumps[i] = _find_jumps_to_scanner(all_mappings, i)
+    return jumps
 
 
 if __name__ == "__main__":
@@ -27,5 +28,20 @@ if __name__ == "__main__":
             if corr_count >= 12:
                 results.append((i, j, corr_count, corr_angle, corr_offset))
 
-    print(f"Part 1: {find_unique_nodes(scannings, results)}\n")
+    jump_map = find_jumps(scannings, results)
+    # Nodes' position relative to origin
+    nodes = set()
+
+    for to, jumps in jump_map.items():
+        for jump in jumps[1:]:
+            # find corresponding in results
+            left, right, _, angles, offset = [v for v in results if jump in v[:2]][0]
+            angles = np.array(angles)
+            offset = np.array(offset)
+            # Invert transform if point of origin was the other one
+            if right == to:
+                angles = -angles
+                offset = -offset
+
+    print(f"Part 1: {find_jumps(scannings, results)}\n")
     print(f"Part 2: {0}\n")
