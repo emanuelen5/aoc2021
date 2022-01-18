7b455b05edef28375b4ee6ca9ab43527b5915153
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-01-18 23:51:03 +0100

    Solution to day 21 part 1

---
+++ days/21/solution.py
@@ -0,0 +1,51 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+parser = ArgumentParser()
+parser.add_argument("--input-file", "-i", default=Path(__file__).parent / "data.txt")
+args = parser.parse_args()
+
+with open(args.input_file) as f:
+    lines = f.read().splitlines()
+
+
+start1 = int(lines[0].split(" ")[-1])
+start2 = int(lines[1].split(" ")[-1])
+
+class DeterministicDice:
+    def __init__(self) -> None:
+        self.throws = 0
+
+    def generate_throws(self):
+        self.throws = 0
+        i = 0
+        while True:
+            self.throws += 1
+            i = i % 100 + 1
+            yield i
+
+
+def play_game(start1: int, start2: int) -> tuple[int, int]:
+    player1_pos = start1
+    player2_pos = start2
+    sum1 = sum2 = 0
+    dice = DeterministicDice()
+    dicer = dice.generate_throws()
+    while True:
+        steps = sum([d for _, d in zip(range(3), dicer)])
+        player1_pos = (player1_pos + steps - 1) % 10 + 1
+        sum1 += player1_pos
+        if sum1 >= 1000:
+            return dice.throws, min(sum1, sum2)
+
+        steps = sum([d for _, d in zip(range(3), dicer)])
+        player2_pos = (player2_pos + steps - 1) % 10 + 1
+        sum2 += player2_pos
+        if sum2 >= 1000:
+            return dice.throws, min(sum1, sum2)
+        
+
+throws, min_score = play_game(start1, start2)
+
+print(f"Part 1: {throws * min_score}\n")
+print(f"Part 2: {0}\n")
